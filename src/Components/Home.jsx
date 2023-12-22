f02bba17e25e0e0b94f151839cadae5de1f1478e
byiringiroaimefils <123945889+byiringiroaimefils@users.noreply.github.com>
2023-12-22 07:50:29 +0200

    v4

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,18 +5,19 @@ import { FaFacebook, FaLinkedinIn, FaTwitter } from "react-icons/fa"
 export default function Home() {
   const Users = data.map(item => {
     return (
-      <div className='flex justify-centeri
-      r64 gap-5 h-50vh align-bottom' key={item.id}>
-        <div>
+      <div className='flex justify-between h-50vh mt-36' key={item.id}>
+        <div className='ml-12 translate-y-28'>
           <FaFacebook />
           <FaLinkedinIn />
           <FaTwitter />
         </div>
-        <div>
+        <div className=' '>
           <img src={item.Image} alt={item.alt} className='w-96' />
         </div>
-        <div>
-          <h2 >{item.Name}</h2>
+        <div className='mr-32 translate-y-20 text-center'>
+          <h2>{item.Greeting}</h2>
+          <h5>{item.Name}</h5>
+          <div className="line w-72 h-1 bg-sky-800 rounded"></div>
           <p dangerouslySetInnerHTML={{ __html: item.Description }} />
 
         </div>

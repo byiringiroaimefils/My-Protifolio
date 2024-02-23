d16ddf7914e825e38028f2b797b2c17c460d2cfc
byiringiroaimefils <aimefils173@gmail.com>
2024-02-23 13:18:57 +0200

    V14

--- src/Components/Achivement.jsx
+++ src/Components/Achivement.jsx
@@ -1,15 +1,13 @@
 import achivementData from "./AchivementData"
 export default function Achivement() {
   return (
-    <div className="Container">
-      <div>
-        {achivementData.map( item => {
-           <div className="box" key={item.id}>
-              <img src={item.Image}/>
-              <p>{item.alt}</p>
+    <div className="Container flex gap-10">
+      {achivementData.map( item => (
+           <div className="text-white" key={item.id}>
+              <img src={item.Image} className="w-96"/>
+              <p className="w-80">{item.alt}</p>
            </div>
-        })}
-      </div>
+        ))} 
     </div>
   )
 }
--- src/Components/AchivementData.jsx
+++ src/Components/AchivementData.jsx
@@ -2,17 +2,17 @@ const achivementData= [
     {
         id: 1,
         Image: 'Javascript Certificate.png', 
-        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quibusdam aut cupiditate earum dolore maiores incidunt officia sapiente optio aliquam.",
+        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
     },
     {
         id: 2,
         Image: 'Javascript Certificate.png', 
-        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quibusdam aut cupiditate earum dolore maiores incidunt officia sapiente optio aliquam.",
+        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. ",
     },
     {
         id: 3,
         Image: 'Javascript Certificate.png', 
-        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quibusdam aut cupiditate earum dolore maiores incidunt officia sapiente optio aliquam.",
+        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
     },
 ]
 

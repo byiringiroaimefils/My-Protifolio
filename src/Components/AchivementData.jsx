fb4cffe870b9ae78ceb95a8bbd4137314c52d1f9
byiringiroaimefils <aimefils173@gmail.com>
2024-02-23 13:20:15 +0200

    V15

--- src/Components/AchivementData.jsx
+++ src/Components/AchivementData.jsx
@@ -2,17 +2,17 @@ const achivementData= [
     {
         id: 1,
         Image: 'Javascript Certificate.png', 
-        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsum dolor.",
     },
     {
         id: 2,
         Image: 'Javascript Certificate.png', 
-        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. ",
+        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsum dolor. ",
     },
     {
         id: 3,
         Image: 'Javascript Certificate.png', 
-        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+        alt: "Lorem ipsum dolor sit amet consectetur, adipisicing elit ipsum dolor.",
     },
 ]
 

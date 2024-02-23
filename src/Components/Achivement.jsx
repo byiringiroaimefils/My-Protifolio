04b2020c312c40153f7fd443a7866346c746c958
byiringiroaimefils <aimefils173@gmail.com>
2024-02-23 13:21:41 +0200

    v17

--- src/Components/Achivement.jsx
+++ src/Components/Achivement.jsx
@@ -5,7 +5,7 @@ export default function Achivement() {
       {achivementData.map( item => (
            <div className="text-white" key={item.id}>
               <img src={item.Image} className="w-96"/>
-              <p className="w-80">{item.alt}</p>
+              <p className="w-96 text-center">{item.alt}</p>
            </div>
         ))} 
     </div>
